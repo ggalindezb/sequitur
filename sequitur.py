2a1bc11aa20a58399fc16dedff034624a41946fb
Gerardo Galindez <ggalindezb@gmail.com>
2019-09-02 22:13:52 -0500

    - Add indention

--- sequitur.py
+++ sequitur.py
@@ -11,7 +11,7 @@ class Sequitur:
         self.set_data()
 
     def set_data(self):
-        path = os.path.join('data.yml')
+        path = os.path.join('dummy.yml')
         contents = open(path, "r").read()
         self.data = yaml.full_load(contents)
         self.print_data()
@@ -20,7 +20,17 @@ class Sequitur:
         for week, day_list in self.data.items():
             print(f'Week: {week}')
             for day, tasks in day_list.items():
-                print(day)
-                print(', '.join([str(x) for x in tasks]))
+                self.print_day(day, tasks)
+
+    def print_day(self, day, tasks):
+        print(f' Day: {day}')
+        self.print_day_tasks(tasks)
+
+    def print_day_tasks(self, tasks):
+        if tasks is not None:
+            task_summary = ', '.join([str(x) for x in tasks])
+            print(f'  {task_summary}')
+        else:
+            print('Nothing to report')
 
 s = Sequitur()
