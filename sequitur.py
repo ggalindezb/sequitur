6555915c984a9c7e643ececad40a15819f8ed957
Gerardo Galindez <ggalindezb@gmail.com>
2019-09-03 22:04:32 -0500

    - Warnings

--- sequitur.py
+++ sequitur.py
@@ -1,3 +1,6 @@
+"""
+Sequitur main entry point
+"""
 #!/usr/local/bin/python3
 
 import os
@@ -5,36 +8,49 @@ import pdb
 import yaml
 
 class Sequitur:
+    LAPSE = {
+        'tiny': 1,
+        'small': 2
+    }
+
     def __init__(self):
         self.init = True
         # pdb.set_trace()
-        self.set_data()
+        self.__set_data__()
 
-    def set_data(self):
+    def __set_data__(self):
         path = os.path.join('dummy.yml')
         contents = open(path, "r").read()
         self.data = yaml.full_load(contents)
-        self.print_data()
 
     # All of this code should be ported to a printer class
     def print_data(self):
+        """Output the current context"""
         for week, day_list in self.data.items():
             print(f'Week: {week}')
             for day, tasks in day_list.items():
-                self.print_day(day, tasks)
+                self.__print_day__(day, tasks)
 
-    def print_day(self, day, tasks):
-        print(f'{self.indent_size(1)}Day: {day}')
-        self.print_day_tasks(tasks)
+    def __print_day__(self, day, tasks):
+        print(f'{self.__indent_size__(1)}Day: {day}')
+        self.__print_day_tasks__(tasks)
 
-    def print_day_tasks(self, tasks):
+    def __print_day_tasks__(self, tasks):
         if tasks is not None:
             task_summary = ', '.join([str(x) for x in tasks])
-            print(f'{self.indent_size(2)}{task_summary}')
+            print(f'{self.__indent_size__(2)}{task_summary}')
         else:
             print('Nothing to report')
 
-    def indent_size(self, index):
+    def __indent_size__(self, index):
         return ' ' * index
+    # Printer -------------------
 
-s = Sequitur()
+    """
+    """
+    def process_report(self):
+        print('Processing report...')
+
+client = Sequitur()
+# client.print_data()
+client.process_report()
